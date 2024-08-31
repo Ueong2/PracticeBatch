f98b4e05414c1fe8b0c2e15efd406f55caeb2ca7
Ueong2 <kiko130@naver.com>
2024-09-01 01:51:50 +0900

    20240901 스케쥴 활용을 위한 실행파일 수정

--- src/main/java/com/example/samplebatch/SampleBatchApplication.java
+++ src/main/java/com/example/samplebatch/SampleBatchApplication.java
@@ -2,10 +2,12 @@ package com.example.samplebatch;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
+@EnableScheduling    // 스케쥴 활용을 위한 어노테이션
 public class SampleBatchApplication {
-
+	
 	public static void main(String[] args) {
 		SpringApplication.run(SampleBatchApplication.class, args);
 	}
