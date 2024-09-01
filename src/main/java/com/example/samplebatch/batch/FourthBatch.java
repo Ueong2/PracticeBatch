5b0c91bfaf6cb518d624dce322cc818a2299fdaf
Ueong2 <kiko130@naver.com>
2024-09-01 23:38:20 +0900

    20240901 DB to Excel 로직 생성

---
+++ src/main/java/com/example/samplebatch/batch/FourthBatch.java
@@ -0,0 +1,83 @@
+package com.example.samplebatch.batch;
+
+import com.example.samplebatch.entity.BeforeEntity;
+import com.example.samplebatch.repository.BeforeRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.batch.core.Job;
+import org.springframework.batch.core.Step;
+import org.springframework.batch.core.job.builder.JobBuilder;
+import org.springframework.batch.core.repository.JobRepository;
+import org.springframework.batch.core.step.builder.StepBuilder;
+import org.springframework.batch.item.ItemProcessor;
+import org.springframework.batch.item.ItemStreamWriter;
+import org.springframework.batch.item.data.RepositoryItemReader;
+import org.springframework.batch.item.data.builder.RepositoryItemReaderBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.domain.Sort;
+import org.springframework.transaction.PlatformTransactionManager;
+
+import java.io.IOException;
+import java.util.Map;
+
+@Configuration
+@RequiredArgsConstructor
+public class FourthBatch {
+
+    private final JobRepository jobRepository;
+    private final PlatformTransactionManager platformTransactionManager;
+    private final BeforeRepository beforeRepository;
+
+    @Bean
+    public Job fourthJob() {
+
+        System.out.println("fourth job");
+
+        return new JobBuilder("fourthJob", jobRepository)
+                .start(fourthStep())
+                .build();
+    }
+
+    @Bean
+    public Step fourthStep() {
+
+        System.out.println("fourth step");
+
+        return new StepBuilder("fourthStep", jobRepository)
+                .<BeforeEntity, BeforeEntity> chunk(10, platformTransactionManager)
+                .reader(fourthBeforeReader())
+                .processor(fourthProcessor())
+                .writer(excelWriter())
+                .build();
+    }
+
+    @Bean
+    public RepositoryItemReader<BeforeEntity> fourthBeforeReader() {
+
+        RepositoryItemReader<BeforeEntity> reader = new RepositoryItemReaderBuilder<BeforeEntity>()
+                .name("beforeReader")
+                .pageSize(10)
+                .methodName("findAll")
+                .repository(beforeRepository)
+                .sorts(Map.of("id", Sort.Direction.ASC))
+                .build();
+
+        // 전체 데이터 셋에서 어디까지 수행 했는지의 값을 저장하지 않음
+        // 엑셀 to DB와 달리 DB to 엑셀은 작업이 중간에 중단되면 파일을 새로 생성해야 하므로
+        // ExcelRowReader의 cursor 저장과 같은 로직의 아래 코드를 false로 한다.
+        reader.setSaveState(false);
+
+        return reader;
+    }
+
+    @Bean
+    public ItemProcessor<BeforeEntity, BeforeEntity> fourthProcessor() {
+        return item -> item;
+    }
+
+    @Bean
+    public ItemStreamWriter<BeforeEntity> excelWriter() {
+        return new ExcelRowWriter("C:\\Users\\User\\Desktop\\result.xlsx");
+        //리눅스나 맥은 /User/형태로
+    }
+}
