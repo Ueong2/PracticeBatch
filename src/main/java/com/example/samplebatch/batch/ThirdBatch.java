68b51d7f4c45cc84bce04a44c248cd3f7673bd93
Ueong2 <kiko130@naver.com>
2024-09-01 15:07:20 +0900

    20240901 Excel to DB 로직 생성

---
+++ src/main/java/com/example/samplebatch/batch/ThirdBatch.java
@@ -0,0 +1,82 @@
+package com.example.samplebatch.batch;
+
+import com.example.samplebatch.entity.AfterEntity;
+import com.example.samplebatch.repository.AfterRepository;
+import lombok.RequiredArgsConstructor;
+import org.apache.poi.ss.usermodel.Row;
+import org.springframework.batch.core.Job;
+import org.springframework.batch.core.Step;
+import org.springframework.batch.core.job.builder.JobBuilder;
+import org.springframework.batch.core.repository.JobRepository;
+import org.springframework.batch.core.step.builder.StepBuilder;
+import org.springframework.batch.item.ItemProcessor;
+import org.springframework.batch.item.ItemStreamReader;
+import org.springframework.batch.item.data.RepositoryItemWriter;
+import org.springframework.batch.item.data.builder.RepositoryItemWriterBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.transaction.PlatformTransactionManager;
+
+import java.io.IOException;
+
+@Configuration
+@RequiredArgsConstructor
+public class ThirdBatch {
+
+    private final JobRepository jobRepository;
+    private final PlatformTransactionManager platformTransactionManager;
+    private final AfterRepository afterRepository;
+
+    @Bean
+    public Job thirdJob() {
+
+        System.out.println("third job");
+
+        return new JobBuilder("thirdJob", jobRepository)
+                .start(thirdStep())
+                .build();
+    }
+
+    @Bean
+    public Step thirdStep() {
+
+        return new StepBuilder("thirdStep", jobRepository)
+                .<Row, AfterEntity> chunk(10, platformTransactionManager)
+                .reader(excelReader())
+                .processor(thirdProcessor())
+                .writer(thirdAfterWriter())
+                .build();
+    }
+
+    @Bean
+    public ItemStreamReader<Row> excelReader() {
+
+        return new ExcelRowReader("C:\\Users\\User\\Desktop\\batchTest.xlsx");
+        //리눅스나 맥은 /User/형태로
+    }
+
+    @Bean
+    public ItemProcessor<Row, AfterEntity> thirdProcessor() {
+
+        return new ItemProcessor<Row, AfterEntity>() {
+
+            @Override
+            public AfterEntity process(Row item) {
+
+                AfterEntity afterEntity = new AfterEntity();
+                afterEntity.setUsername(item.getCell(0).getStringCellValue());
+
+                return afterEntity;
+            }
+        };
+    }
+
+    @Bean
+    public RepositoryItemWriter<AfterEntity> thirdAfterWriter() {
+
+        return new RepositoryItemWriterBuilder<AfterEntity>()
+                .repository(afterRepository)
+                .methodName("save")
+                .build();
+    }
+}
